46cbfc995e4860ea9aa3e669d68c13caed147b68
fuuki7 <iitsuka728@gmail.com>
2020-12-17 20:56:18 +0900

    bot(Mark.4.2)
    ジャービス化

--- bot.py
+++ bot.py
@@ -4,89 +4,85 @@ from mutagen.mp3 import MP3 as mp3
 import pygame
 import time
 import win32com.client as wincl
-voice = wincl.Dispatch("SAPI.SpVoice")
 
+voice = wincl.Dispatch("SAPI.SpVoice")
 times = datetime.datetime.now()
-ai = "アイ＞＞"
+ai = "ジャービス＞＞"
+ti = times.hour
+hen = ["どうかしましたか？" , "大丈夫ですか？" , "何か仕事はありますか？"]
+l = ["大吉","中吉","小吉","吉","末吉","凶","大凶"]
+s = "きっといいことがあるでしょう"
+x = "運勢など気にせず頑張ってください"
+z ={"大吉": s ,"中吉": s ,"小吉": s ,"吉": s ,"末吉": s ,"凶": x ,"大凶": x}
+eotw = ["Dropout Boulevard.mp3" , "Bad Day.mp3" , "Fangs.mp3" , "Forever.mp3" , "Gone.mp3" , "My Sleeping Beauty.mp3" , "Hollow.mp3" , "Over.mp3" , "Rollerskates.mp3" , "Stargazer.mp3"]
 
 def tim():
-    print(ai + "今の時間は" + str(times.hour) + "時" + str(times.minute) + "分だよ")
-    voice.Speak("今の時間は" + str(times.hour) + "時" + str(times.minute) + "分だよ")
+    print(ai + "今の時間は" + str(times.hour) + "時" + str(times.minute) + "分です")
+    voice.Speak("今の時間は" + str(times.hour) + "時" + str(times.minute) + "分です")
 
 def year():
-    print(ai + "今日は" + str(times.month) + "月" + str(times.day) + "日だよ")
-    voice.Speak("今日は" + str(times.month) + "月" + str(times.day) + "日だよ")
-
-ti = times.hour
-
-hen = ["どうかした？" , "おーい" , "なんか話してよー"]
-l = ["大吉","中吉","小吉","吉","末吉","凶","大凶"]
-s = "きっといいことあるよ！！"
-x = "運勢なんて気にせず頑張ろう！！"
-z ={"大吉": s ,"中吉": s ,"小吉": s ,"吉": s ,"末吉":"いいことあるといいね！！","凶": x ,"大凶": x}
+    print(ai + "今日は" + str(times.month) + "月" + str(times.day) + "日です")
+    voice.Speak("今日は" + str(times.month) + "月" + str(times.day) + "日です")
 
 def omi():
     a = random.randint(0,6)
     v = l[a]
-    print(ai + "今日のあなたの運勢は" + v + "だよ!\n" + ai + z[v])
-    voice.Speak("今日のあなたの運勢は" + v + "だよ!\n" + z[v])
+    print(ai + "今日の" + names + "の運勢は" + v + "です\n" + ai + z[v])
+    voice.Speak("今日の" + names + "の運勢は" + v + "です\n" + z[v])
 
 def jan():
     ff = "じゃんけん終了"
-    print(ai + "じゃんけん大会スタート！")
-    voice.Speak("じゃんけん大会スタート！")
-    print(ai + "番号を入力してね")
-    voice.Speak("番号を入力してね")
+    print(ai + "じゃんけん大会スタートです")
+    voice.Speak("じゃんけん大会スタートです")
+    print(ai + "番号を入力してください")
+    voice.Speak("番号を入力してください")
     print("[1.グー　2.チョキ　3.パー]")
     ja = input("番号:")
     jb = int(ja)
     jc = random.randint(1,3)
     if jb==1:
         if jc==1:
-            print(ai + "グー　　\n" + ai + "あいこだよ")
-            voice.Speak("あいこだよ")
+            print(ai + "グー　　\n" + ai + "あいこです")
+            voice.Speak("あいこです")
             print(ff)
         elif jc==2:
-            print(ai + "チョキ \n" + ai + name + "の勝ちだよ")
-            voice.Speak( name + "の勝ちだよ")
+            print(ai + "チョキ \n" + ai + name + "の勝ちです")
+            voice.Speak( name + "の勝ちです")
             print(ff)
         else:
-            print(ai + "パー \n" + ai + "私の勝ちだよ！")
-            voice.Speak("私の勝ちだよ！")
+            print(ai + "パー \n" + ai + "私の勝ちです")
+            voice.Speak("私の勝ちです")
             print(ff)
     elif jb==2:
         if jc==1:
-            print(ai + "グー\n" + ai + "私の勝ちだよ！")
-            voice.Speak("私の勝ちだよ！")
+            print(ai + "グー\n" + ai + "私の勝ちです")
+            voice.Speak("私の勝ちです")
             print(ff)
         elif jc==2:
-            print(ai + "チョキ \n" + ai + "あいこだよ")
-            voice.Speak("あいこだよ")
+            print(ai + "チョキ \n" + ai + "あいこです")
+            voice.Speak("あいこです")
             print(ff)
         else:
-            print(ai + "パー \n" + ai + name + "の勝ちだよ")
-            voice.Speak( name + "の勝ちだよ")
+            print(ai + "パー \n" + ai + name + "の勝ちです")
+            voice.Speak( name + "の勝ちです")
             print(ff)
     elif jb==3:
         if jc==1:
-            print(ai + "グー \n" + ai + name + "の勝ちだよ")
-            voice.Speak( name + "の勝ちだよ")
+            print(ai + "グー \n" + ai + name + "の勝ちです")
+            voice.Speak( name + "の勝ちです")
             print(ff)
         elif jc==2:
-            print(ai + "チョキ \n" + ai + "私の勝ちだよ！")
-            voice.Speak("私の勝ちだよ！")
+            print(ai + "チョキ \n" + ai + "私の勝ちです")
+            voice.Speak("私の勝ちです")
             print(ff)
         else:
-            print(ai + "パー \n" + ai + "あいこだよ")
-            voice.Speak("あいこだよ")
+            print(ai + "パー \n" + ai + "あいこです")
+            voice.Speak("あいこです")
             print(ff)
     else:
-        print(ai + "1から3の整数を選んでね")
-        voice.Speak("1から3の整数を選んでね")
+        print(ai + "1から3の整数を選んでください")
+        voice.Speak("1から3の整数を選んでください")
         print(ff)
-
-
-eotw = ["Dropout Boulevard.mp3" , "Bad Day.mp3" , "Fangs.mp3" , "Forever.mp3" , "Gone.mp3" , "My Sleeping Beauty.mp3" , "Hollow.mp3" , "Over.mp3" , "Rollerskates.mp3" , "Stargazer.mp3"]
 
 def music(ongaku):
     filename = ongaku
@@ -100,20 +96,23 @@ def music(ongaku):
 print("会話開始")
 
 if ti <= 9:
-    print(ai + "おはよう！")
-    voice.Speak("おはよう")
+    print(ai + "おはようございます")
+    voice.Speak("おはようございます")
 elif ti >9 and ti < 19:
-    print(ai + "こんにちわ！")
+    print(ai + "こんにちわ")
     voice.Speak("こんにちわ")
 else:
-    print(ai + "こんばんわ！")
+    print(ai + "こんばんわ")
     voice.Speak("こんばんわ")
 
-print("アイ＞＞私はアイ\nアイ＞＞君のお名前は？")
-voice.Speak("私はアイ、君のお名前は？")
+print("ジャービス＞＞私はジャービス\nジャービス＞＞あなたの生活のサポートをさせていただきます")
+voice.Speak("私はジャービス、あなたの生活のサポートをさせていただきます")
+print(ai +  'あなたのお名前を教えてください')
+voice.Speak("あなたのお名前を教えてください")
 name = input("名前＞ ")
-print(ai + name + "っていうんだ、よろしくね！")
-voice.Speak( name + "っていうんだ、よろしくね！")
+names = name + '様'
+print(ai + names + "ですね。よろしくお願いします。")
+voice.Speak( names + "ですね。よろしくお願いします。")
 pro = name + ">"
 
 
@@ -121,18 +120,18 @@ def h():
     cunt = 0
     while 1:
         an = input(pro)
-        if an == "こんにちわ" or an == "おはよう" or an == "こんばんわ":
+        if an == "こんにちわ" or an == "おはようございます" or an == "こんばんわ":
             print(ai + an + "!")
             voice.Speak(an)
 
         elif an == "ばいばい" or an == "さよなら":
             if ti >= 19:
-                print(ai + "またお話ししようね！\n" + ai + "おやすみ！" )
-                voice.Speak( "またお話ししようね！おやすみ！" )
+                print(ai + "また何かあれば呼んでください\n" + ai + "おやすみなさい" )
+                voice.Speak( "また何かあれば呼んでください、おやすみなさい" )
                 break
             else:
-                print(ai + "ばいばい！\n" + ai + "またお話ししようね！")
-                voice.Speak("ばいばい！またお話ししようね！")
+                print(ai + "さようなら\n" + ai + "また何かあれば呼んでください")
+                voice.Speak("さようなら、また何かあれば呼んでください")
                 break
 
         elif not an:
@@ -143,8 +142,8 @@ def h():
             print("無視回数" + str(cunt))
             
             if cunt == 8:
-                print(ai + "もう行くね")
-                voice.Speak("もう行くね")
+                print(ai + "また用があるときに呼んでください")
+                voice.Speak("また用があるときに呼んでください")
                 break
 
         elif an == "おみくじ":
@@ -158,13 +157,13 @@ def h():
 
         elif an == "じゃんけんしよ":
             jan()
-            print(ai + "もう一回やる？")
-            voice.Speak("もう一回やる？")
+            print(ai + "もう一度やりますか？？")
+            voice.Speak("もう一度やりますか？？")
             dd = input("はい、いいえ＞")
             while dd == "はい":
                 jan()
-                print(ai + "もう一回やる？")
-                voice.Speak("もう一回やる？")
+                print(ai + "もう一度やりますか？？")
+                voice.Speak("もう一度やりますか？？")
                 dd = input("はい、いいえ＞")
         
         elif an == "音楽かけて":
@@ -173,8 +172,8 @@ def h():
             music(eotw[m])
 
         else:
-            print(ai + "{}なんだね！".format(an))
-            voice.Speak("{}なんだね！".format(an))
+            print(ai + "{}なんですね".format(an))
+            voice.Speak("{}なんですね".format(an))
 
 h()
 
